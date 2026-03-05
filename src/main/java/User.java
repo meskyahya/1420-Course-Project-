4b5be7f9a24462e0a533a5d63c67800ed23987e6
navinar08 <navina.r08@gmail.com>
2026-03-04 20:03:34 -0500

    Update User.java

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,48 +1,37 @@
+package com.useraddition.usermanagement;
+
 public class User {
 
+    private int id;
     private String name;
     private String email;
-    private int userId;
-    private String role;
 
-    public User( String name, String email, int userId, String role){
+    public User(int id, String name, String email){
+        this.id = id;
         this.name = name;
         this.email = email;
-        this.userId = userId;
-        this.role = role;}
-
-
+    }
+    public int getId(){
+        return id;}
+    public void serId(int id){
+        this.id = id;}
     public String getname(){
-        return name;
-    }
-
-    public void setname(String name){
-        this.name = name;
-    }
-
+        return name;}
+    public void setname( String name){
+        this.name = name;}
     public String getemail(){
-        return email;
-    }
+        return email;}
+    public void setemail(String email){
+        this.email = email; }
 
-    public void setemail( String email){
-       this.email = email;
-    }
-
-    public int getuserID(){
-        return userId;
-    }
-
-    public void setuserId(int userId){
-        this.userId = userId;
-    }
     public String getrole(){
-        return role;
+        return "User";
     }
 
-    public void setrole( String role){
-        this.role = role;
-    }
+    public String toString(){
+        return "|" + id + "|" + name + "|" + email + "|" + getrole();}
 
 
 }
+
 
