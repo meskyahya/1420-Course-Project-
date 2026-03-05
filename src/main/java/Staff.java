df24996e436c4b153a87b6de5f8cd8f590a3cd45
navinar08 <navina.r08@gmail.com>
2026-03-04 20:04:39 -0500

    Update Staff.java
    Refactor Staff class to update constructor and add getrole method.

--- src/main/java/Staff.java
+++ src/main/java/Staff.java
@@ -1,9 +1,14 @@
+package com.useraddition.usermanagement;
+
 public class Staff extends User {
-    public Staff(String name, String email, int userId) {
-        super(name, email, userId,"Staff");
+
+    public Staff(int id, String name, String email){
+        super(id, name, email);
     }
 
-
-
+    public String getrole(){
+        return "Staff";
+    }
 }
+
 
