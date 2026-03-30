e9e5d31b7feb296cd5ccddd9f51926c569cbd41e
meskyahya <yahyamesk@gmail.com>
2026-03-29 20:39:22 -0400

    Fix syntax error

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,6 +1,6 @@
 public class Student extends User {
 
-    public Student(String name, String email, String userId)
+    public Student(String name, String email, String userId) {
         super(name, email, userId, "Student");
     }
 }
