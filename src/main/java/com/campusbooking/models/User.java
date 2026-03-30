4a35d36c44c0abd6e7b41318a81fd287678e2067
navinar08 <navina.r08@gmail.com>
2026-03-30 10:19:42 -0400

    Added comments to the User

--- src/main/java/com/campusbooking/models/User.java
+++ src/main/java/com/campusbooking/models/User.java
@@ -1,24 +1,68 @@
+// main package which groups all the model classes together)
 package com.campusbooking.models;
 
+// creating a class for the user object 
 public class User {
+
+    // creating a field to store the user name
     private String name;
+
+    // creating a field to store the user email
     private String email;
+
+    // creating a field to store userID
     private String userId;
+
+    // creating a field to store the role
     private String role;
 
+    // creating a constructor which runs if the user object is created and the parameters are the input values that is used to set fields
     public User(String name, String email, String userId, String role) {
+
+        // sets name field
         this.name = name;
+
+        // sets email field
         this.email = email;
+
+        // sets userId field
         this.userId = userId;
+
+        // sets role field
         this.role = role;
     }
 
-    public String getname() { return name; }
-    public void setname(String name) { this.name = name; }
-    public String getemail() { return email; }
-    public void setemail(String email) { this.email = email; }
-    public String getuserID() { return userId; }
-    public void setuserId(String userId) { this.userId = userId; }
-    public String getrole() { return role; }
-    public void setrole(String role) { this.role = role; }
+    // getter and setters
+
+    // creating a method to return name
+    public String getName() { 
+        return name; }
+    
+    // creating a method to update name
+    public void setName(String name) { 
+        this.name = name; }
+
+   // creating a method to return email
+    public String getEmail() {
+        return email; }
+    
+    // creating a method to update email
+    public void setEmail(String email) { 
+        this.email = email; }
+
+    // creating a method to userId
+    public String getuserID() { 
+        return userId; }
+    
+    // creating a method to update userID
+    public void setuserId(String userId) { 
+        this.userId = userId; }
+
+    // creating a method to return role    
+    public String getrole() { 
+        return role; }
+    
+    // creating a method to update role
+    public void setrole(String role) { 
+        this.role = role; }
 }
