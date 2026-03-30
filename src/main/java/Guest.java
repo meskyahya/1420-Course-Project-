0db119b37fa96189c8c19ff0a894b3d716459ce7
meskyahya <yahyamesk@gmail.com>
2026-03-29 20:39:43 -0400

    Fix syntax error

--- src/main/java/Guest.java
+++ src/main/java/Guest.java
@@ -1,5 +1,5 @@
 public class Guest extends User{
-    public Guest(String name, String email, String userId)
+    public Guest(String name, String email, String userId) {
         super(name, email, userId, "guest");
     }
 
