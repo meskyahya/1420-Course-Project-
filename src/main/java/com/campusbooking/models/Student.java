7f38d889a412e3f589a66fdcfe807abb4d9d542c
meskyahya <yahyamesk@gmail.com>
2026-03-29 21:18:25 -0400

    Change userId type from int to String in Student

--- src/main/java/com/campusbooking/models/Student.java
+++ src/main/java/com/campusbooking/models/Student.java
@@ -2,8 +2,8 @@ package com.campusbooking.models;
 
 public class Student extends User {
 
-    public Student(String name, String email, int userId){
-        super(name, email, userId, "Student");
+    public Student(String name, String email, String userId) {
+    super(name, email, userId, "Student");
     }
 }
 
